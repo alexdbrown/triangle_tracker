7b5b448433e92f77e87bf148717cdd9055533eb5
alexandra <student@epicodus.com>
2015-09-10 15:40:30 -0700

    finish passing tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 var triangleTracker = function(num1, num2, num3) {
-    if ((num1 + num2) <= (num3)) ||
+    if (((num1 + num2) <= (num3)) ||
        ((num2 + num3) <= (num1)) ||
-       ((num3 + num1) <= (num2)) {
-    return "not a triangle";
-}
+       ((num3 + num1) <= (num2))) {
+       return "not a triangle";
+
+  } else if (num1 === num2 && num2 === num3){
+       return "equilateral";
+
+  } else if (num1 === num2 || num1 === num3 || num2 === num3){
+       return "isosceles";
+
+  } else {
+       return "scalene";
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,19 @@
-describe('traingleTracker', function() {
+describe('triangleTracker', function() {
     it("is not a triangle if the sum of two sides is less than the third", function() {
-        expect(triangleTracker(1, 3, 1).to.equal("not a triangle"));
+        expect(triangleTracker(1, 3, 1)).to.equal("not a triangle");
     });
 
+    it("is equilateral when all sides are equal", function() {
+    expect(triangleTracker(2, 2, 2)).to.equal("equilateral");
+    });
 
+    it("is isosceles when exactly two sides are equal", function() {
+    expect(triangleTracker(2, 2, 1)).to.equal("isosceles");
+    });
 
-  // it("is equilateral when all sides are equal", function() {
-  //   expect(traingleTracker(2, 2, 2).to.equal("equilateral"));
-  // });
+    it("is scalene when no sides are equal", function() {
+    expect(triangleTracker(2, 4, 5)).to.equal("scalene");
+    });
+
 
 });
